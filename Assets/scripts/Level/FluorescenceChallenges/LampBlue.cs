b84c6e8b80895ff89260654f2dac6592af68da82
Ben <benjamin.brogniart@gmail.com>
2013-06-18 17:37:31 +0200

    edit fluro challenge

--- Assets/scripts/Level/FluorescenceChallenges/LampBlue.cs
+++ Assets/scripts/Level/FluorescenceChallenges/LampBlue.cs
@@ -5,17 +5,23 @@ public class LampBlue : MonoBehaviour {
 
 	public Color colorGreen = Color.green;
 
-	void OnTriggerStay(Collider col) {
+	void OnTriggerEnter(Collider col) {
 		if (col.gameObject.name == "Hero") {
 			col.gameObject.light.enabled = true;
 			col.gameObject.light.color = colorGreen;
 		}
-		GameObject door = GameObject.Find("DoorGreen");
-		door.transform.Translate(0, 0, 1 * Time.deltaTime);
+		//GameObject door = GameObject.Find("DoorGreen");
+		//door.transform.Translate(0, 0, 2 * Time.deltaTime);
+		DoorScript door = GameObject.Find("DoorGreen").GetComponent<DoorScript>();
+		//door.toggleState();
+		door.openDoor();
 	}
 
 	void OnTriggerExit(Collider col) {
 		if (col.gameObject.name == "Hero")
 			col.gameObject.light.enabled = false;
+		DoorScript door = GameObject.Find("DoorGreen").GetComponent<DoorScript>();
+		//door.toggleState();
+		door.closeDoor();
 	}
 }
